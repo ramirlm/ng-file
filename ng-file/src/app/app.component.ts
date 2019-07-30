41796b638b299529e6db4374518b1eb7bdc2b6ee
Ramir Mesquita <ramir.mesquita@gmail.com>
2019-07-30 18:31:42 -0300

    First version of upload app

---
+++ ng-file/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-file';
+  uploadedFiles: Array < File > ;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  fileChange(element) {
+      this.uploadedFiles = element.target.files;
+  }
+
+  upload() {
+    let formData = new FormData();
+    formData.append('file', this.uploadedFiles[0], this.uploadedFiles[0].name);
+
+    this.http.post('http://localhost:8080/', formData)
+    .subscribe((response) => {
+         console.log('response received is ', response);
+    });
+  }
+
+}
